74c766753152048ccfa4eb5d340e1edd527d67f6
rene <prene@tlu.ee>
2025-10-23 17:12:12 +0300

    fix(supabase): update client configuration

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,11 +1,9 @@
-import { createClient as createSupabaseClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
-export const createPublicClient = () => {
-  return createSupabaseClient(
+export function createPublicClient() {
+  return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
   );
-};
-
-// Add alias for backward compatibility:
+}
 export const createClient = createPublicClient;
